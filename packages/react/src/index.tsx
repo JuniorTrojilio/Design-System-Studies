c9b26e78a659ce39ca135786fe05c33c270a9904
Junior Trojilio <osmar.trojilio@gmail.com>
2023-01-05 15:33:46 -0300

    feat: create exports from index

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,34 +1,3 @@
-import { ComponentProps } from 'react'
-import { styled } from './styles'
-
-export const Button = styled('button', {
-  fontFamily: '$default',
-  backgroundColor: '$ignite300',
-  borderRadius: '$md',
-  padding: '$4',
-  border: 'none',
-  color: '$white',
-
-  variants: {
-    size: {
-      sm: {
-        fontSize: '$sm',
-        padding: '$2',
-      },
-      md: {
-        fontSize: '$md',
-        padding: '$4',
-      },
-      lg: {
-        fontSize: '$lg',
-        padding: '$6',
-      },
-    },
-  },
-
-  defaultVariants: {
-    size: 'md',
-  },
-})
-
-export type ButtonProps = ComponentProps<typeof Button>
+export * from './components/Box'
+export * from './components/Heading'
+export * from './components/Text'
